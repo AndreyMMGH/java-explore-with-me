e1ffabb842184992f13472eed1e7ea2da8797295
Андрей <mazurenkoam.93@yandex.ru>
2025-08-15 15:21:44 +0300

    Покрыть тестами API для работы с категориями

---
+++ main/src/test/java/ru/practicum/CategoryAdminControllerTest.java
@@ -0,0 +1,92 @@
+package ru.practicum;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import ru.practicum.category.controller.CategoryAdminController;
+import ru.practicum.category.dto.CategoryDto;
+import ru.practicum.category.dto.NewCategoryDto;
+import ru.practicum.category.service.CategoryService;
+import ru.practicum.error.ErrorHandler;
+
+import java.nio.charset.StandardCharsets;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.hamcrest.Matchers.is;
+
+@ExtendWith(MockitoExtension.class)
+class CategoryAdminControllerTest {
+
+    private final ObjectMapper mapper = new ObjectMapper();
+    @Mock
+    private CategoryService categoryService;
+    @InjectMocks
+    private CategoryAdminController categoryAdminController;
+    private MockMvc mvc;
+    private CategoryDto categoryDto;
+    private NewCategoryDto newCategoryDto;
+
+    @BeforeEach
+    void setUp() {
+        mapper.findAndRegisterModules();
+        mvc = MockMvcBuilders
+                .standaloneSetup(categoryAdminController)
+                .setControllerAdvice(new ErrorHandler())
+                .build();
+
+        categoryDto = new CategoryDto(1L, "Концерты");
+        newCategoryDto = new NewCategoryDto("Концерты");
+    }
+
+    @Test
+    void mustCreateCategory() throws Exception {
+        when(categoryService.createCategory(any()))
+                .thenReturn(categoryDto);
+
+        mvc.perform(post("/admin/categories")
+                        .content(mapper.writeValueAsString(newCategoryDto))
+                        .characterEncoding(StandardCharsets.UTF_8)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.id", is(1)))
+                .andExpect(jsonPath("$.name", is("Концерты")));
+    }
+
+    @Test
+    void mustDeleteCategory() throws Exception {
+        mvc.perform(delete("/admin/categories/1"))
+                .andExpect(status().isNoContent());
+
+        verify(categoryService).deleteCategory(1L);
+    }
+
+    @Test
+    void mustUpdateCategory() throws Exception {
+        CategoryDto updated = new CategoryDto(1L, "Фестивали");
+        when(categoryService.updateCategory(eq(1L), any(NewCategoryDto.class)))
+                .thenReturn(updated);
+
+        mvc.perform(patch("/admin/categories/1")
+                        .content(mapper.writeValueAsString(newCategoryDto))
+                        .characterEncoding(StandardCharsets.UTF_8)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id", is(1)))
+                .andExpect(jsonPath("$.name", is("Фестивали")));
+    }
+}
---
+++ main/src/test/java/ru/practicum/CategoryMapperTest.java
@@ -0,0 +1,31 @@
+package ru.practicum;
+
+import org.assertj.core.api.AssertionsForClassTypes;
+import org.junit.jupiter.api.Test;
+import ru.practicum.category.dto.CategoryDto;
+import ru.practicum.category.dto.NewCategoryDto;
+import ru.practicum.category.dto.mapper.CategoryMapper;
+import ru.practicum.category.model.Category;
+
+
+public class CategoryMapperTest {
+    @Test
+    void shouldMapCategoryToCategoryDto() {
+        Category category = new Category(1L, "Кино");
+
+        CategoryDto result = CategoryMapper.toCategoryDto(category);
+
+        AssertionsForClassTypes.assertThat(result.getId()).isEqualTo(1L);
+        AssertionsForClassTypes.assertThat(result.getName()).isEqualTo("Кино");
+    }
+
+    @Test
+    void shouldMapNewCategoryDtoToCategory() {
+        NewCategoryDto newCategoryDto = new NewCategoryDto("Кино");
+
+        Category result = CategoryMapper.toCategory(newCategoryDto);
+
+        AssertionsForClassTypes.assertThat(result.getId()).isNull();
+        AssertionsForClassTypes.assertThat(result.getName()).isEqualTo("Кино");
+    }
+}
